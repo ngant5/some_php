65e55d7e5ea3011b5dad80abc2a21464f44c7e9f
nga <nguyentuyetnga0209@gmail.com>
2019-12-12 14:47:22 +0700

    add view page

--- view.php
+++ view.php
@@ -1,9 +1,12 @@
+<style>
+    table, th, td{border: 1px solid grey};
+</style>
 <?php
-require "./session.php";
+//require "./session.php";
 require "./connection.php";
 
-$conn = $conn_db();
-$row = '';
+$conn = conn_db();
+$row[] = '';
 
 if(isset($_GET["id"]) && $_GET["id"] > 0) {
     $id = $_GET["id"];
@@ -12,14 +15,39 @@ if(isset($_GET["id"]) && $_GET["id"] > 0) {
 
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
+        ?>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>PRODUCT NAME</th>
+                <th>QUANLITY</th>
+                <th>STATUS</th>
+                <th>ACTION</th>
+                <th>USER NAME</th>
+            </tr>
+            <tr>
+                <td><?=$row['id'] ?></td>
+                <td><?=$row['name'] ?></td>
+                <td><?=$row['quanlity'] ?></td>
+                <td><?=$row['status'] == 1 ? "Active" : "Deactive" ?></td>
+                <td>
+                    <a href="<?="view.php?id={$row['id']}" ?>"> View </a> ||
+                    <a href="<?="edit.php?id={$row['id']}" ?>"> Edit </a> ||
+                    <a href="<?="delete.php?id={$row['id']}" ?>"> Delete </a>
+                </td>
+                <td><?=$row['user'] ?></td>
+            </tr>
+        </table>
+        <?php
+        } else {
+        header("Location: http://localhost/some_php/login.php");
+        die();
+        }
     } else {
-        header("Location: http://localhost/some_php/login.php");
-        die();
-    }
-} else {
-        header("Location: http://localhost/some_php/login.php");
-        die();
-}
+        echo "loi";
+                header("Location: http://localhost/some_php/login.php");
+                die();
+        }
 
 mysqli_close($conn);
 
